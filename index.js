e8fc15f42e3e65e8814fd8aeba351a120fa7c051
Hengki Sihombing <hengki.sihombing@gmail.com>
2017-01-29 19:51:33 +0700

    fixing get domain and tld with tldjs

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-(function(module) {
-  'use strict';
 
-  var Url = require('url');
+'use strict'
 
-  module.exports = function parseURL(url) {
+const Url = require('url')
+const tldjs = require('tldjs')
 
-      var parsed_url = Url.parse(url);
-
-      if(parsed_url.hostname == null) {
-        var domain_parts = parsed_url.pathname.split('.');
-      } else {
-        var domain_parts = parsed_url.hostname.split('.');
-      }
-
-      switch ( domain_parts.length ){
-          case 2:
-            parsed_url.subdomain = null;
-            parsed_url.host = domain_parts[0];
-            parsed_url.tld = domain_parts[1];
-            parsed_url.domain = domain_parts[0] + '.' + domain_parts[1];
-            break;
-          case 3:
-            parsed_url.subdomain = domain_parts[0];
-            parsed_url.host = domain_parts[1];
-            parsed_url.tld = domain_parts[2];
-            parsed_url.domain = domain_parts[1] + '.' + domain_parts[2];
-            break;
-          case 4:
-            parsed_url.subdomain = domain_parts[0];
-            parsed_url.host = domain_parts[1];
-            parsed_url.tld = domain_parts[2] + '.' + domain_parts[3];
-            parsed_url.domain = domain_parts[1] + '.' + parsed_url.tld;
-            break;
-      }
-
-      delete parsed_url.auth;
-      delete parsed_url.search;
-      delete parsed_url.query;
-      delete parsed_url.href;
-      delete parsed_url.hash;
-      delete parsed_url.slashes;
-      delete parsed_url.pathname;
-      return parsed_url
+function addhttp (url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = 'http://' + url
   }
-})(module);
+  return url
+}
+
+module.exports = (url) => {
+  if (typeof url !== 'string') {
+    url = ''
+  }
+
+  const makeAsUrl = addhttp(url)
+  const parsedUrl = Url.parse(makeAsUrl)
+
+  if (!parsedUrl.protocol) parsedUrl.protocol = 'http'
+
+  parsedUrl.domain = tldjs.getDomain(parsedUrl.hostname || parsedUrl.host)
+
+  delete parsedUrl.auth
+  delete parsedUrl.search
+  // delete parsedUrl.query
+  delete parsedUrl.href
+  // delete parsedUrl.hash
+  delete parsedUrl.slashes
+  delete parsedUrl.pathname
+  return parsedUrl
+}
